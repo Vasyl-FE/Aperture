78f30565a07d3637c0030aebba84f392f64e06af
Vasyl S <sukhovoy.v@gmail.com>
2022-08-20 09:09:13 +0300

    Adaptive finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        navText: [ '', ' ' ],
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: ['', ' '],
 
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             },
+            375: {items: 1}
+,            428: {items: 2},
+            768: {items: 3},
+            820: {items: 3},
 
-            1000:{
-                items:5
+            992: {items: 4},
+            1200: {
+                items: 5
             }
         }
     });
